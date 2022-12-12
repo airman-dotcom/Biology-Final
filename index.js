45af49040b378721ea42317a5a743153e61e3904
airman-dotcom <amathakbari@gmail.com>
2022-12-11 22:06:10 -0800

    Day 2

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html")
 })
 
-app.listen(3000, () => {
+app.get("/functions", (req, res) => {
+  res.sendFile(__dirname + "/public/functions.html");
+})
+
+app.listen(3000, "localhost", () => {
   console.log("Welcome to the Respiratory System.")
 })
