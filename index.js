b9f5f0801c52be31d86218881c6a9253f1f28bb9
Replit user <>
2022-12-14 01:30:22 +0000

    2 days left

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ app.get("/functions", (req, res) => {
   res.sendFile(__dirname + "/public/functions.html");
 })
 
+app.get("/anatomy", (req, res) => {
+  res.sendFile(__dirname + "/public/anatomy.html");
+})
+
+app.get("/homeostasis", (req, res) => {
+  res.sendFile(__dirname + "/public/homeostasis.html");
+})
+
+app.get("/disease-disorders", (req, res) => {
+  res.sendFile(__dirname + "/public/disease-disorders.html")
+})
+
+app.get("/bibliography", (req, res) => {
+  res.sendFile(__dirname + "/public/bibliography.html");
+})
+
 app.listen(3000, () => {
   console.log("Welcome to the Respiratory System.")
 })
